952154f7adb9f6babc0e70ea863399689d2ffa0f
Pro gupta <pchemistry.35@gmail.com>
2024-12-19 16:58:31 +0530

    chore: udpated

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const orderValidation = [
   body('planId').isString().notEmpty(),
   body('name').isString().notEmpty(),
-  body('phone').isString(),
+  body('phone').isString().optional(),
   body('email').isEmail().notEmpty(),
 ];
 
